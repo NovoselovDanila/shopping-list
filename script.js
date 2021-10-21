e76353a5a4437c86b026833396bf45a9dfbed173
NovoselovDanila <danila_loye@mail.ru>
2021-10-21 14:28:44 +0300

    done
    done

---
+++ script.js
@@ -0,0 +1,51 @@
+//Задача1
+// function mnoj(a,b){
+//     return a*b
+// }
+// console.log(mnoj(2,6))
+//Задача2
+// function fio(name,fam,age){
+//     console.log(`Привет ${name+' '+ fam} с возрастом ${age} лет!`)
+// }
+// fio('Данила','Новосёлов',18)
+//Задача3
+// function pol(gender){
+//     if (gender.toUpperCase() === 'M'){
+//         console.log('Ваш пол мужской!')
+//     } else if (gender.toUpperCase() === 'F'){
+//         console.log('Ваш пол женский!')
+//     }
+// }
+// pol('f')
+//Задача4
+// function mes(num){
+//     const mas = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье']
+//     console.log(mas[num-1])
+// }
+// mes(1)
+//Задача5
+// function abs(str){
+//     let mas = str.split('.')
+//     for (index = 0; index<mas.length;index++){
+//         let masTemp = mas[index].split(' ')
+//         console.log(masTemp[0])
+//     }
+// }
+// abs('Всем привет.Привет всем.Здравствуйте.')
+//Задача6
+// function age(){
+//     let age = 1
+//     for (age = 1; age<=100;age++){
+//         if (age>=1&&age<=17){
+//             console.log(age+' - ребёнок')
+//         } else if (age>=18&&age<=30){
+//             console.log(age+' - молодой')
+//         } else if (age>=31&&age<=55){
+//             console.log(age+' - зрелый')
+//         } else if (age>55){
+//             console.log(age+' - старый')
+//         }
+//     }
+// }
+// age()
+//Задача7
